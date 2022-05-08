6eb3b4b14dbb8b5f01dcdf9e1502a9b7e662fad8
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2022-05-08 23:17:21 +0900

    d

---
+++ copy.md
@@ -0,0 +1,19 @@
+# 写経を楽にしたい
+
+# 作業手順
+
+# dir 作成
+シェルスクリプトで考える
+pdfにまとめてディレクトリが記述されている
+
+mkdirとtouch
+mkdirは被っているとエラー
+
+# pdf を textファイルに
+コンソールでtextファイルに変換する
+apt install poppler-utils
+pdftotext -layout -nopgbrk "PDFファイル"
+
+ailasに登録
+
+
---
+++ mkdir.sh
@@ -0,0 +1,23 @@
+
+mkdir phpdb
+mkdir phpdb/chap{01..04}
+
+touch phpdb/index.html
+touch phpdb/chap01/connectToDB.php
+touch phpdb/chap01/failedToConnect.php
+touch phpdb/chap01/failedToConnectWithTryCatch.php
+touch phpdb/chap02/insertData.php
+touch phpdb/chap02/updateData.php
+touch phpdb/chap02/deleteData.php
+touch phpdb/chap03/findManyRecords.php
+touch phpdb/chap03/findOneRecords.php
+touch phpdb/chap03/findWithLike.php
+
+mkdir phpdb/chap04/findone && touch phpdb/chap04/findone/Order.php
+touch phpdb/chap04/findone/findOneWithEntity.php
+mkdir phpdb/chap04/findmany && touch phpdb/chap04/findmany/Order.php
+touch phpdb/chap04/findmany/findManyWithEntity.php
+
+# 実行
+# 実行階層 xampp/htdogs/ph35/mkdir.sh
+# 実行コマンド
