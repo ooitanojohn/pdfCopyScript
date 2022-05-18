ab04a9b93045afe61772f0deee972fecca01a0dd
ooitanojohn <qmkh733310@gmail.com>
2022-05-19 01:13:13 +0900

    ほぼshellなのにruby判定されるのはまずい

---
+++ dist/func/test/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 実行先のshellを決める
+
+# スクリプト名
+echo $0
+# 引数の数
+echo $#
+# PCのプロセスID
+echo $$
+# 引数
+echo $1
+# すべての引数
+echo $*
+#
+echo $@
---
+++ format.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# pdf→txtに
+pdftotext -layout -nopgbrk $1 format.txt
+# 行末変換
+sed -i -e "s/¬//g" format.txt
+# 行頭の数字削除 (dist.sh)
+rm dist.txt
+cat format.txt | while read line
+do
+  echo "${line:3}" >> dist.txt
+done
+rm format.txt
+# ファイル分割
+rm -rf split
+mkdir split
+# -s 出力時コンソール出さない url[https://linuxcommand.net/csplit/#_-s]
+csplit -s -z -f split/split -b '%02u.txt' dist.txt '/^nted: 2022/*/' '{*}'
+# 1行と最終行コメントを削除
+sed -i -e "1d" split/split*.txt
+sed -i -e '$d' split/split*.txt
+
+# ファイル開始時にコメントがないファイルは (cat.sh)
+# ひとつ上の数字のファイルと結合
+# bash url[https://qiita.com/akinomyoga/items/9761031c551d43307374]
+grep -L "ファイル名=" split/split**.txt >cat.txt
+# 10進数 [https://kazmax.zpp.jp/cmd/e/expr.1.html#ah_4]
+cat cat.txt | while read line
+do
+  num=${line:11:-4}
+  num=`expr $num - 1`
+  if [ $num -gt 0 ] ; then
+    num=$(printf "%02d" $num)
+    cat $line >>split/split$num.txt
+    rm $line
+  fi
+done
+
+# ファイルrename (rename.sh)
+grep "ファイル名=" split/*.txt > rename.txt
+cat rename.txt | while read line
+do
+  mv ${line%%:*} ${line%%/*}/${line##*ファイル名=}
+done
+
+#フォルダ一覧作成 [共通部分を手動で作る必要がある為その部分を取得して自動作成したい、重複をifで実行しないようにしたい] && 移動 (mkdir.sh)
+grep "フォルダ=/" split/* >mkdir.txt
+
+sed -i -e "s/split\///g" mkdir.txt
+cat mkdir.txt | while read line
+do
+  mkdir "${line##*フォルダ=/}"
+  mv split/${line%%:*} ${line##*フォルダ=/}${line%%:*}
+done
+
